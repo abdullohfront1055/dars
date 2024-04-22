3d29797c5675a0d0ed4eb0da07a055ed6ee1c420
Abdulloh Xo'jayev <xodjaevabdulloh54@gmail.com>
2024-04-22 11:15:36 +0500

    tugadi

---
+++ index.js
@@ -0,0 +1,31 @@
+let word = prompt("Ingliz tilida soz kiriting");
+
+if(word === "monday"){
+    alert("Dushanba");
+}
+
+if(word === "thuesday"){
+    alert("Seyshanba");
+}
+
+if(word === "Wednesday"){
+    alert("Chorshanba");
+}
+
+if(word === "Thursday"){
+    alert("Payshanba");
+}
+
+if(word === "Friday"){
+    alert("Juma");
+}
+
+if(word === "Satyrday"){
+    alert("Shanba");
+}
+
+if(word === "Sanday"){
+    alert("Yakshanba");
+}
+
+
